5524a5d5ae3608ed9a04d54894948031dfb9a047
Thiago Valverde de Souza <valverde.thiago@gmail.com>
2025-03-18 23:42:34 -0300

    Startup issue fixed

--- src/main/kotlin/com/valverdethiago/kus/service/XXHashService.kt
+++ src/main/kotlin/com/valverdethiago/kus/service/XXHashService.kt
@@ -1,7 +1,9 @@
 package com.valverdethiago.kus.service
 
 import net.openhft.hashing.LongHashFunction
+import org.springframework.stereotype.Service
 
+@Service
 class XXHashService : HashService {
     override fun hash(input: String): String {
         val hashValue = LongHashFunction.xx().hashChars(input)
